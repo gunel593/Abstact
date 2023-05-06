ab262c4872e319b19b769aacd0e58c29aa48684b
Admin <Admin@DESKTOP-TT8HLOQ>
2023-05-06 22:07:47 +0400

    Add project files.

---
+++ Abstact/Class1.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleApp7;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp7
+{
+    public abstract class Tree
+    {
+        protected int pricePerTree;
+        protected int countofFruit; public abstract int OverallPriceOfFruits();
+        public Tree(int countOfFruit, int pricePerTree)
+        {
+            this.countofFruit = countOfFruit; this.pricePerTree = pricePerTree;
+        }
+    }
+    public class AppleTree : Tree
+    {
+        public override int OverallPriceOfFruits()
+        {
+            return countofFruit * pricePerTree;
+        }
+        public AppleTree(int countOfFruit, int pricePerTree) : base(countOfFruit, pricePerTree)
+        {
+            this.countofFruit = countOfFruit; this.pricePerTree = pricePerTree;
+        }
+    }
+
+    public class BananaTree : Tree
+    {
+        public override int OverallPriceOfFruits()
+        {
+            return countofFruit * pricePerTree;
+        }
+        public BananaTree(int countOfFruit, int pricePerTree) : base(countOfFruit, pricePerTree)
+        {
+            this.countofFruit = countOfFruit;
+            this.pricePerTree = pricePerTree;
+        }
+    }
+    public class LemonTree : Tree
+    {
+        public override int OverallPriceOfFruits()
+        {
+            return countofFruit * pricePerTree;
+        }
+        public LemonTree(int countOfFruit, int pricePerTree) : base(countOfFruit, pricePerTree)
+        {
+            this.countofFruit = countOfFruit;
+            this.pricePerTree = pricePerTree;
+        }
+    }
+}
---
+++ Abstact/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppleTree AppleTree = new AppleTree(8, 6); AppleTree.OverallPriceOfFruits();
+            Console.WriteLine(AppleTree.OverallPriceOfFruits()); BananaTree BananaTree = new BananaTree(7, 9);
+            BananaTree.OverallPriceOfFruits(); Console.WriteLine(BananaTree.OverallPriceOfFruits());
+            LemonTree LemonTree = new LemonTree(9, 6); LemonTree.OverallPriceOfFruits();
+            Console.WriteLine(LemonTree.OverallPriceOfFruits());
+        }
+    }
+}
